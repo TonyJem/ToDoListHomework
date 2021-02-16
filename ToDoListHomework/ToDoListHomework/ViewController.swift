9c66bf3a73ebb080832fb770f35713aeef8e6f61
TonyJem <a.jemanov@gmail.com>
2021-02-16 10:35:33 +0200

    Add TableView to ViewController Programmatically

--- ToDoListHomework/ToDoListHomework/ViewController.swift
+++ ToDoListHomework/ToDoListHomework/ViewController.swift
@@ -1,9 +1,49 @@
+// Ideas from:
+// https://dev.to/amclv/building-a-uitableview-programmatically-4e3m
+// https://softauthor.com/ios-uitableview-programmatically-in-swift/
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        addSubviewsToMainView()
+        setConstraintsToTableView()
+    }
+}
+
+extension ViewController {
+    func addSubviewsToMainView() {
+        view.addSubview(tableView)
+    }
+
+    func setConstraintsToTableView() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
     }
 }
