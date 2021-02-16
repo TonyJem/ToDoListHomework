215ec0338c86de56ce98377e1f20f791e69c8c50
TonyJem <a.jemanov@gmail.com>
2021-02-16 14:11:14 +0200

    Remove storyBoard fully

--- ToDoListHomework/ToDoListHomework/SceneDelegate.swift
+++ ToDoListHomework/ToDoListHomework/SceneDelegate.swift
@@ -4,22 +4,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
-    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        if let windowScene = scene as? UIWindowScene {
-            
-            let window = UIWindow(windowScene: windowScene)
-            let navController = UINavigationController()
-            let viewController = ViewController()
-            
-            navController.viewControllers = [viewController]
-            window.rootViewController = navController
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+        let window = UIWindow(windowScene: windowScene)
+        let navController = UINavigationController()
+        let viewController = ViewController()
         
-        guard let _ = (scene as? UIWindowScene) else { return }
+        navController.viewControllers = [viewController]
+        window.rootViewController = navController
+        
+        self.window = window
+        window.makeKeyAndVisible()
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
